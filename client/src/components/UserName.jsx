edc99865ed0cda1a8af8899afc530b8f900ce3f1
AnasJMirza <ajmhimself@gmail.com>
2023-01-27 00:47:53 +0500

    login-ui-setup

--- client/src/components/UserName.jsx
+++ client/src/components/UserName.jsx
@@ -1,27 +1,53 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import profilePrview from '../assets/profile.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import profilePrview from "../assets/profile.png";
+import styles from "../styles/UserName.module.css";
 
 const UserName = () => {
-    return (
-        <div className='flex flex-col justify-center items-center gap-2'>
-            <div className='font-bold text-4xl'>Login</div>
-            <div className='py-4 text-xl w-2/3 text-center text-gray-500'>Unlock new possibilities by connecting with us</div>
-            <form className='form'>
-                <div className='profile flex justify-center'>
-                    <img src={profilePrview} alt="profile" className='w-28'/>
-                </div>
-                <div className='textbox flex flex-col gap-2'>
-                    <input type="text" placeholder='username'/>
-                    <button type='submit'>Let's go!</button>
-                </div>
-                <span className='flex gap-2'>
-                    <p className='text-gray-500'>Not a Memeber</p>
-                    <Link to='/register' className='text-red-500 font-medium'>Register Now</Link>
-                </span>
-            </form>
+  return (
+    <div className="container  mx-auto">
+
+      <div className="flex justify-center items-center h-screen">
+
+        <div className={styles.glass}>
+
+          <div className="title flex flex-col items-center">
+            <h4 className="text-3xl font-bold">Hello Again!</h4>
+            <span className="py-4 text-md w-3/4 text-center text-gray-500">
+              Explore More by connecting with us.
+            </span>
+          </div>
+
+          <form className="form">
+
+            <div className="profile flex justify-center pt-2 pb-4">
+              <img
+                src={profilePrview}
+                alt="profile"
+                className={styles.profile_img}
+              />
+            </div>
+
+            <div className="textbox flex flex-col items-center gap-6">
+              <input
+                className={styles.textbox}
+                type="text"
+                placeholder="username"
+              />
+              <div className={'border bg-indigo-500 hover:bg-[#ff6a6a] w-3/4 py-2 rounded-lg text-gray-50 text-xl shadow-sm text-center cursor-pointer'} type="submit">
+                Let's go!
+              </div>
+            </div>
+
+            <div className="text-center py-4">
+                <span className='text-gray-500'>Not a Member <Link className='text-red-500' to="/register">Register Now</Link></span>
+            </div>
+
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default UserName;

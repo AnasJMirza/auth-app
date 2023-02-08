3602f356782cbacd8c1ce62cfe24219c17b30fd6
AnasJMirza <ajmhimself@gmail.com>
2023-02-09 01:24:01 +0500

    resetSession-route

--- server/routes/otp.js
+++ server/routes/otp.js
@@ -1,16 +1,15 @@
 import express from 'express';
-import { generateOTP, verifyOTP } from '../controllers/otp.js';
+import { generateOTP, resetSession, verifyOTP } from '../controllers/otp.js';
 import { verifyUser } from '../middlewares/verifyUser.js';
 
 
 const router = express.Router();
 
 
-
-
 // GET routes
 router.get('/generate-otp', verifyUser, generateOTP);
 router.get('/verify-otp', verifyUser, verifyOTP);
+router.get('/reset-session', resetSession);
 
 
 
